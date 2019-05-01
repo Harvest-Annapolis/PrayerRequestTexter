dd28298ec5413572af650ad3577b27a24794cf7d
harvestannapolis <jhinton@harvestannapolis.org>
2019-05-01 03:22:47 +0100

    removed some extra stuff

--- send_message.py
+++ send_message.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from twilio.rest import Client
-from secure import auth_token
+from secure import auth_token, account_sid, account_number
 def send_message(to, content):
-    account_sid = "AC28f029f16dbfe40db0e3282072153ecc"
+    account_sid = account_sid
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         to=to, 
-        from_="+14432929313",
+        from_=account_number,
         body=content)
     return message.status
