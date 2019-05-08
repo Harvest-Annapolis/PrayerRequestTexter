1cacf1dc5b42e18587a22e866f53aac668f8cf92
Sean Atwell <sta0003@auburn.edu>
2019-05-07 23:00:34 -0400

    Testing twilio

--- web_application.py
+++ web_application.py
@@ -1,6 +1,7 @@
 #To Run: gunicorn --certfile server.crt --keyfile server.key -b 0.0.0.0:8443 web_application:app
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def index():
     Renders the homepage.
     """
     return render_template('index.html')
+    
+@app.route("/test_info", methods=["POST"])
+def test_twilio():
+    f = open("/tmp/testme.txt", "w")
+    f.write(json.dumps(request))
+    f.close()
+    
 
 if __name__ == "__main__":
     app.run()
