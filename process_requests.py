8e1e1116f973dff78ae456d6e36b08e0612903b8
Sean Atwell <sta0003@auburn.edu>
2019-05-22 23:25:24 -0400

    Testing

--- process_requests.py
+++ process_requests.py
@@ -4,11 +4,13 @@ from send_message import send_message
 from db_repo import *
 import datetime
 
+print("Here")
 custs = Customer.select().where(Customer.enabled and 
                                   Customer.last_run_date < datetime.date.today() and 
                                   Customer.execute_time < datetime.datetime.now().time())
 prayer_request = get_message()
 
+print(len(custs))
 for cust in custs:    
     cust.last_run_date = datetime.date.today()
     cust.save()
