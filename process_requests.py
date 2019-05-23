1aba558c8206ea7f0b818273f4c9acf6d540dd90
Sean Atwell <sta0003@auburn.edu>
2019-05-22 22:48:35 -0400

    Added features and migrated to the database for sending and stuff.

--- process_requests.py
+++ process_requests.py
@@ -2,10 +2,17 @@
 from parse_requests import get_message
 from send_message import send_message
 from db_repo import *
+import datetime
 
-numbers = [ "+13347968921" , "+15867702894" , "+16315990273" , "+14107034138" ]
+#numbers = [ "+13347968921" , "+15867702894" , "+16315990273" , "+14107034138" ]
+custs = Customer.select().where(Customer.enabled and 
+                                  Customer.last_run_date < datetime.date.today() and 
+                                  Customer.execute_time < datetime.datetime.now().time())
 prayer_request = get_message()
 
-for phone in numbers:
-    status = send_message(phone, prayer_request)
+for cust in custs:    
+    cust.last_run_date = datetime.date.today()
+    cust.save()
+    
+    status = send_message(cust.phone_number, prayer_request)
     print(status)
--- web_application.py
+++ web_application.py
@@ -16,19 +16,29 @@ def index():
     
 @app.route("/test_info", methods=["POST"])
 def test_twilio():
+    # Parse data from request.
     phone_number = request.form["phone_number"]
     message_content = request.form["message_content"]
     
+    # Variable defaults
     send_welcome = False
+    
+    #Get the customer that's texting.
     cust = Customer.get_or_none(Customer.phone_number == phone_number) 
+    
+    # Make a new customer if one doesn't exist.
     if cust == None:
         cust = Customer(phone_number=phone_number)    
         cust.save(force_insert=True)
         send_welcome = True
-        
+                
+    # Re-enable a previously disabled customer.
     if not cust.enabled and ("yes" in message_content.lower() or "subscribe" in message_content.lower()):
         send_welcome = True
-    
+        cust.enabled = True
+        cust.save()
+        
+    # Parse out the time they want to receive texts    
     time_value = "".join([i for i in message_content if i.isdigit()])
     if len(time_value) <= 6:
         good_time = False
@@ -50,13 +60,24 @@ def test_twilio():
 
         if not send_welcome:
             status = send_message(cust.phone_number, "You have adjusted the time you receive the daily prayer text to {}.".format(cust.execute_time.strftime("%I:%M %p")))
+    # Unsubscribe
     elif "stop" in message_content.lower() or "unsubscribe" in message_content.lower():
         cust.enabled = False
         cust.save()
+    # Command not recognized
     elif not send_welcome:
         status = send_message(cust.phone_number, "Sorry, I don't recognize that command.")
+    # Send a welcome message if they havent gotten one before.
     elif send_welcome:
         status = send_message(cust.phone_number, "Thank you for subscribing to the Harvest Prayer Request text list.  You will currently receive a daily prayer request at {}.  To change this time, simply reply with the time you would prefer.  To stop these messages, simply reply STOP.".format(cust.execute_time.strftime("%I:%M %p")))
+    
+    # Make sure they get today's message if the time they picked hasn't passed yet.
+    if datetime.datetime.now() < datetime.datetime.combine(datetime.datetime.now().date(), cust.execute_time):
+        cust.last_run_date = datetime.date.today() - datetime.timedelta(days=1)
+        cust.save()
+    else:
+        cust.last_run_date = datetime.date.today()
+        cust.save()
     
     return render_template('success.html') 
 
