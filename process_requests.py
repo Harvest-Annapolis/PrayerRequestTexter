17aff2c63ee2a77e0b07441aa428d9fbf0760f3f
Sean Atwell <sta0003@auburn.edu>
2019-05-22 23:31:04 -0400

    I really hate peewee sometimes...

--- process_requests.py
+++ process_requests.py
@@ -4,16 +4,15 @@ from send_message import send_message
 from db_repo import *
 import datetime
 
-print("Here")
-custs = Customer.select().where(Customer.enabled and 
-                                  Customer.last_run_date < datetime.date.today() and 
-                                  Customer.execute_time < datetime.datetime.now().time())
+custs = Customer.select().where(Customer.enabled & 
+                                (Customer.last_run_date < datetime.date.today()) & 
+                                (Customer.execute_time < datetime.datetime.now().time()))
 prayer_request = get_message()
 
-print(len(custs))
 for cust in custs:    
     cust.last_run_date = datetime.date.today()
     cust.save()
     
-    status = send_message(cust.phone_number, prayer_request)
+    #status = send_message(cust.phone_number, prayer_request)
+    status = "Run"
     print(status)
