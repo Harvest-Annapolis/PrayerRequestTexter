e83f785880099da3f2b3c847e282341a473bc325
Sean Atwell <sta0003@auburn.edu>
2019-05-22 22:59:19 -0400

    removing hard-coded numbers

--- process_requests.py
+++ process_requests.py
@@ -4,7 +4,6 @@ from send_message import send_message
 from db_repo import *
 import datetime
 
-#numbers = [ "+13347968921" , "+15867702894" , "+16315990273" , "+14107034138" ]
 custs = Customer.select().where(Customer.enabled and 
                                   Customer.last_run_date < datetime.date.today() and 
                                   Customer.execute_time < datetime.datetime.now().time())
