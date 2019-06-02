a8c7b330b2a3b2a506e87474122a8aaec9ed3720
Sean Atwell <sta0003@auburn.edu>
2019-06-01 20:50:36 -0400

    Adding a webform to sign up for texts

--- web_application.py
+++ web_application.py
@@ -19,10 +19,13 @@ def test_twilio():
     # Parse data from request.
     phone_number = request.form["phone_number"]
     message_content = request.form["message_content"]
+    web_submission = request.form["web_submission"]
+    time_value = "".join([i for i in message_content if i.isdigit()])
     
     # Variable defaults
     send_welcome = False
     welcome_string = "Thank you for subscribing to the Harvest Prayer Request text list.  You will currently receive a daily prayer request at {}.  To change this time, simply reply with the time you would prefer.  To stop these messages, simply reply STOP."
+    web_new_string = "We've just received word that you want to sign up to receive the Harvest Annapolis daily prayer text at {}.  If that's the case, please text START to confirm.  Thank you."
     update_time_string = "You have adjusted the time you receive the daily prayer text to {}."
     bad_command_string = "Sorry, I don't recognize that command."
     
@@ -40,9 +43,9 @@ def test_twilio():
         send_welcome = True
         cust.enabled = True
         cust.save()
-        
+       
+            
     # Parse out the time they want to receive texts    
-    time_value = "".join([i for i in message_content if i.isdigit()])
     if len(time_value) <= 6 and len(time_value) > 0:
         good_time = False
         if len(time_value) in [1, 2]:
@@ -66,6 +69,15 @@ def test_twilio():
             status = send_message(cust.phone_number, update_time_string.format(cust.execute_time.strftime("%I:%M %p")))
         else:    
             status = send_message(cust.phone_number, welcome_string.format(cust.execute_time.strftime("%I:%M %p")))
+            
+        # Make sure they get today's message if the time they picked hasn't passed yet.
+        if datetime.datetime.now() < datetime.datetime.combine(datetime.datetime.now().date(), cust.execute_time):
+            cust.last_run_date = datetime.date.today() - datetime.timedelta(days=1)
+            cust.save()
+        else:
+            cust.last_run_date = datetime.date.today()
+            cust.save()
+    
     # Unsubscribe
     elif "stop" in message_content.lower() or "unsubscribe" in message_content.lower():
         cust.enabled = False
@@ -75,15 +87,12 @@ def test_twilio():
         status = send_message(cust.phone_number, bad_command_string)
     # Send a welcome message if they havent gotten one before.
     elif send_welcome:
-        status = send_message(cust.phone_number, welcome_string.format(cust.execute_time.strftime("%I:%M %p")))
-    
-    # Make sure they get today's message if the time they picked hasn't passed yet.
-    if datetime.datetime.now() < datetime.datetime.combine(datetime.datetime.now().date(), cust.execute_time):
-        cust.last_run_date = datetime.date.today() - datetime.timedelta(days=1)
-        cust.save()
-    else:
-        cust.last_run_date = datetime.date.today()
-        cust.save()
+        if web_submission:
+            status = send_message(cust.phone_number, web_new_string.format(cust.execute_time.strftime("%I:%M %p")))
+            cust.enabled = False
+            cust.save()
+        else
+            status = send_message(cust.phone_number, welcome_string.format(cust.execute_time.strftime("%I:%M %p")))    
     
     return render_template('success.html') 
 
